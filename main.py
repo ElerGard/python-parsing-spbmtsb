b651405879759f112d6a325c14e8185c87e69446
ElerGard <norfick@mail.ru>
2022-07-19 13:42:22 +1000

    checking for new files and dowbloading them

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ def getFilesFromPage(r, folder_to_save_files):
     links = r.html.find(".page-content__tabs__block:first-child .accordeon-inner__item-title")
     for link in links:
         file_link = "https://spimex.com" + link.attrs['href']
-        filepath = folder_to_save_files + '/' + re.search("\w*[.]\w+", link.attrs["href"])[0]
+        path_to_file = folder_to_save_files + '/' + re.search("\w*[.]\w+", link.attrs["href"])[0]
+        
         if not(os.path.isdir(folder_to_save_files)): 
             os.makedirs(folder_to_save_files)
             
-        request.urlretrieve(file_link, filepath)
-    return
+        if os.path.exists(path_to_file):
+            return False
+        request.urlretrieve(file_link, path_to_file)
+    return True
     
 def getFilesFromAllPages(url, folder_to_save_files):
     page_number = 1
@@ -23,7 +26,9 @@ def getFilesFromAllPages(url, folder_to_save_files):
         page_url = url + str(page_number)
         r = session.get(page_url)
         print("url: " + page_url)
-        getFilesFromPage(r, folder_to_save_files)
+        if not(getFilesFromPage(r, folder_to_save_files)):
+            print("Files downloaded")
+            return
         page_number += 1
         sleep(20)
         if (r.html.find(".bx-pag-next > a") == []):
@@ -32,6 +37,6 @@ def getFilesFromAllPages(url, folder_to_save_files):
     return
 
 def main():
-    getFilesFromAllPages("https://spimex.com/markets/oil_products/trades/results/?page=page-", "Downloads2")
+    getFilesFromAllPages("https://spimex.com/markets/oil_products/trades/results/?page=page-", "Downloads")
     
 main()
