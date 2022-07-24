a8cf0978e85955a998086550cb660fefa7071e29
ElerGard <norfick@mail.ru>
2022-07-25 05:16:55 +1000

    change GUI and add sleep between dowloading files in main

---
+++ GUI.pyw
@@ -0,0 +1,247 @@
+from tkinter import *
+import pyodbc
+import os
+import pandas as pd
+from datetime import datetime, date
+from tkcalendar import DateEntry
+   
+def autocompleteFirst(event):
+    value = e.get()
+
+    if value == "":
+        data = unselected_tools
+    else:
+        data = []
+        for item in unselected_tools:
+            if value.lower() in item.lower():
+                data.append(item)
+
+    lb.delete(0, "end")
+    for item in data:
+        lb.insert("end", item)
+
+def autocompleteSecond(event):
+    value = e2.get()
+
+    if value == "":
+        data = unselected_basis
+    else:
+        data = []
+        for item in unselected_basis:
+            if value.lower() in item.lower():
+                data.append(item)
+
+    lb3.delete(0, "end")
+    for item in data:
+        lb3.insert("end", item)
+  
+def add_tool(event):
+    cs = lb.curselection()[0]
+    selected_item = lb.get(0, "end")[cs]
+    lb2.insert("end", selected_item)
+    unselected_tools.remove(selected_item)
+    unselected_tools.sort()
+    
+    autocompleteFirst(event)
+
+def add_basis(event):
+    cs = lb3.curselection()[0]
+    selected_item = lb3.get(0, "end")[cs]
+    lb4.insert("end", selected_item)
+    unselected_basis.remove(selected_item)
+    unselected_basis.sort()
+    
+    autocompleteSecond(event)
+    
+def del_tool(event):
+    cs = lb2.curselection()[0]
+    
+    unselected_tools.append(lb2.get(0, "end")[cs])
+    unselected_tools.sort()
+    lb2.delete(cs, cs)
+    
+    autocompleteFirst(event)
+
+def del_basis(event):
+    cs = lb4.curselection()[0]
+    
+    unselected_basis.append(lb4.get(0, "end")[cs])
+    unselected_basis.sort()
+    lb4.delete(cs, cs)
+    
+    autocompleteSecond(event)
+
+def reset_selected_tools():
+    global unselected_tools
+    
+    e.delete(0, "end")
+    lb.delete(0, "end")
+    lb2.delete(0, "end")
+    
+    for item in all_tools:
+        lb.insert("end", item)
+        
+    unselected_tools = all_tools.copy()
+
+def reset_selected_basis():
+    global unselected_basis
+    
+    e2.delete(0, "end")
+    lb3.delete(0, "end")
+    lb4.delete(0, "end")
+    
+    for item in all_basis:
+        lb3.insert("end", item)
+        
+    unselected_basis = all_basis.copy()
+    
+def write_to_excel(excel_name, df):
+    if os.path.exists(excel_name):
+        with pd.ExcelWriter(excel_name,mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
+            df.to_excel(writer, sheet_name="Sheet1",header=None, startrow=writer.sheets["Sheet1"].max_row,index=False)
+    else:
+        writer = pd.ExcelWriter(excel_name, engine="openpyxl")
+        df.to_excel(writer, index=False)
+        writer.save()
+    
+def export():
+    isFinded = False
+    now = datetime.now()
+    date_time = "Экспорт " + now.strftime("%d.%m.%Y_%H.%M")
+    lbl3.configure(text="Экспорт данных...")
+    for tool in lb2.get(0, "end"):
+        for basis in lb4.get(0, "end"):
+            cursor.execute(f"select * from Главная where Товар = '{tool}' and БазисПоставки = '{basis}' and СреднЦена <> 0 and Дата between #{date_from.get()}# and #{date_to.get()}#")
+            res = cursor.fetchall()
+            if (res == []):
+                continue
+            isFinded = True
+            df = pd.DataFrame({"Код инструмента": [item[1] for item in res],
+                            "Наименование инструмента": [item[2] for item in res],
+                            "Сокращенное название": [item[16] for item in res],
+                            "Базис поставки": [item[3] for item in res],
+                            "Объем Договоров в тоннах": [item[4] for item in res],
+                            "Объем Договоров, руб.": [item[5] for item in res],
+                            "Средневзвешенная": [item[9] for item in res],
+                            "Лучшее предложение": [item[12] for item in res],
+                            "Лучший спрос": [item[13] for item in res],
+                            "Количество Договоров, шт.": [item[14] for item in res],
+                            "Дата": [item[15] for item in res]})
+            df["Дата"] = pd.to_datetime(df["Дата"], format="%d.%m.%Y")
+            write_to_excel(f"Export\{date_time}.xlsx", df)
+            lbl3.configure(text="Экспорт завершен")
+    if not(isFinded):
+        lbl3.configure(text="Нет соответствий")
+    return
+    
+def update():
+    for item in all_tools:
+        lb.insert("end", item)
+        
+    for item in all_basis:
+        lb3.insert("end", item)
+
+try:
+    conn = pyodbc.connect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\db.accdb;")
+    cursor = conn.cursor()
+    cursor.execute("select distinct БазисПоставки from Главная where БазисПоставки <> ''")
+    all_basis = [item[0] for item in cursor.fetchall()]
+
+    cursor.execute("select distinct Товар from Главная where Товар <> ''")
+    all_tools  = [item[0] for item in cursor.fetchall()]
+
+    all_tools = sorted(all_tools)
+    all_basis = sorted(all_basis)
+
+    unselected_tools = all_tools.copy()
+
+    unselected_basis = all_basis.copy()
+    
+    if not(os.path.isdir("Export")): 
+            os.makedirs("Export")
+
+    root = Tk()
+    root.title("SPB")
+    root.geometry("575x600")
+
+    scrollbar = Scrollbar(root)
+    scrollbar.grid(row=2, column=1, sticky="ns")
+
+    scrollbar2 = Scrollbar(root)
+    scrollbar2.grid(row=2, column=5, sticky="ns")
+
+    scrollbar3 = Scrollbar(root)
+    scrollbar3.grid(row=5, column=1, sticky="ns")
+
+    scrollbar4 = Scrollbar(root)
+    scrollbar4.grid(row=5, column=5, sticky="ns")
+
+    lbl = Label(root, text="Название\nинструмента")
+    lbl.grid(column=0, row=0)
+
+    e = Entry(root, width=35)
+    e.grid(column=0, row=1)
+    e.bind("<KeyRelease>", autocompleteFirst)
+    
+    lb = Listbox(root, height=10, width=35, yscrollcommand=scrollbar.set)
+    lb.grid(column=0, row=2)
+    lb.bind("<Double-1>", add_tool)
+
+    button = Button(root, text="Сброс", command=reset_selected_tools)
+    button.grid(column=2, row=2, padx=33)
+
+    lb2 = Listbox(root, height=10, width=35, yscrollcommand=scrollbar2.set)
+    lb2.grid(column=4, row=2)
+    lb2.bind("<Double-1>", del_tool)
+
+    #-------------------
+    lbl2 = Label(root, text="Базис\nпоставки")
+    lbl2.grid(column=0, row=3)
+
+    e2 = Entry(root, width=35)
+    e2.grid(column=0, row=4)
+    e2.bind("<KeyRelease>", autocompleteSecond)
+    
+    lb3 = Listbox(root, height=10, width=35, yscrollcommand=scrollbar3.set)
+    lb3.grid(column=0, row=5)
+    lb3.bind("<Double-1>", add_basis)
+
+    button2 = Button(root, text="Сброс", command=reset_selected_basis)
+    button2.grid(column=2, row=5)
+
+    lb4 = Listbox(root, height=10, width=35,  yscrollcommand=scrollbar4.set)
+    lb4.grid(column=4, row=5)
+    lb4.bind("<Double-1>", del_basis)
+
+    lbl4 = Label(root, text="Дата от")
+    lbl4.grid(column=0, row=8)
+
+    date_from = DateEntry(root, selectmode="day", date_pattern="mm/dd/y", mindate=date(2015, 10, 19), maxdate=date.today(), year=2015,month=10,day=19)
+    date_from.grid(column=0, row=9)
+
+    lbl5 = Label(root, text="Дата до")
+    lbl5.grid(column=0, row=10)
+
+    date_to = DateEntry(root, selectmode="day", date_pattern="mm/dd/y", mindate=date(2015, 10, 19), maxdate=date.today())
+    date_to.grid(column=0, row=11)
+
+    button3 = Button(root, text="Экспорт", command=export)
+    button3.grid(column=2)
+
+    lbl3 = Label(root, text="")
+    lbl3.grid(column=2)
+
+    scrollbar.config(command=lb.yview)
+    scrollbar2.config(command=lb2.yview)
+    scrollbar3.config(command=lb3.yview)
+    scrollbar4.config(command=lb4.yview)
+
+    update()
+    
+    root.resizable(0, 0)
+    root.mainloop()
+    cursor.close()
+    conn.close() 
+    
+except pyodbc.Error as e:
+    print("DB not found")
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from cmath import nan
-from msilib.schema import Directory
 from time import sleep
 from requests_html import HTMLSession
 from urllib import request
@@ -8,66 +6,6 @@ import os
 import pyodbc
 import pandas as pd
 
-# class elem:
-#     def __init__(self,
-#                  tool_code, 
-#                  tool_name, 
-#                  delivery_basis, 
-#                  scope_of_contracts_ed, 
-#                  scope_of_contracts_rub,
-#                  rub,
-#                  persent,
-#                  minimal,
-#                  weighted_average,
-#                  maximal,
-#                  market,
-#                  best_offer, 
-#                  best_demand,
-#                  number_of_contracts
-#                 ):
-#         self.tool_code = tool_code
-#         self.tool_name = tool_name
-#         self.delivery_basis = delivery_basis
-#         self.scope_of_contracts_ed = scope_of_contracts_ed 
-#         self.scope_of_contracts_rub = scope_of_contracts_rub
-#         self.rub = rub
-#         self.persent = persent
-#         self.minimal = minimal
-#         self.weighted_average = weighted_average
-#         self.maximal = maximal
-#         self.market = market
-#         self.best_offer = best_offer 
-#         self.best_demand = best_demand
-#         self.number_of_contracts = number_of_contracts
-   
-# one = [
-#     ["Контролер поставки"],
-#     ["Код биржевого товара", "руб"],
-#     ["Код биржевого товара","Руб."],
-#     ["Код\nИнструмента", "Базис поставки"],
-#     ["Код инструмента"],
-#     ["Код\nИнструмента", "Объем Договоров в тоннах"],
-#     ["Код\nИнструмента", "Объем\nДоговоров\nв единицах\nизмерения"]
-# ]
-
-# # TODO
-# def typeOfTable(xls):
-#     if not(xls.loc[xls[xls.columns[0]] == one[0][0]].empty):
-#         return 1
-#     if not(xls.loc[xls[xls.columns[0]] == one[1][0]].empty) and not(xls.loc[xls[xls.columns[6]] == one[1][1]].empty):
-#         return 2
-#     if not(xls.loc[xls[xls.columns[0]] == one[2][0]].empty) and not(xls.loc[xls[xls.columns[5]] == one[2][1]].empty):
-#         return 3
-#     if not(xls.loc[xls[xls.columns[0]] == one[3][0]].empty) and not(xls.loc[xls[xls.columns[2]] == one[3][1]].empty):
-#         return 4
-#     if not(xls.loc[xls[xls.columns[0]] == one[4][0]].empty):
-#         return 5
-#     if not(xls.loc[xls[xls.columns[0]] == one[5][0]].empty) and not(xls.loc[xls[xls.columns[3]] == one[5][1]].empty):
-#         return 6
-#     if not(xls.loc[xls[xls.columns[0]] == one[6][0]].empty) and not(xls.loc[xls[xls.columns[3]] == one[6][1]].empty):
-#         return 7
-#     return 0
-    
 def getFilesFromPage(r, folder_to_save_files):
     links = r.html.find(".page-content__tabs__block:first-child .accordeon-inner__item-title")
     for link in links:
@@ -81,6 +19,7 @@ def getFilesFromPage(r, folder_to_save_files):
             return False
         request.urlretrieve(file_link, path_to_file)
         print(re.search("\w*[.]\w+", link.attrs["href"])[0] + " dowloaded")
+        sleep(3)
     return True
     
 def getFilesFromAllPages(url, folder_to_save_files):
@@ -122,62 +61,64 @@ def insertDB(directory, db_filename):
         "АИ-98-К5-Евро",
         "Мазут"
     ]   
-    
-    conn = pyodbc.connect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\\" + db_filename + ";")
-    cursor = conn.cursor()
-    
-    for filename in reversed(os.listdir(directory)):
-        path_to_file = os.path.join(directory, filename)
-        xls = pd.ExcelFile(path_to_file)
-        sheetX = xls.parse(0) 
+    try:
+        conn = pyodbc.connect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\\" + db_filename + ";")
+        cursor = conn.cursor()
         
-        if sheetX.empty:
-            print('File is empty!')
-            continue
-        
-        old_date = re.search("\d{8}", filename)[0]
-        date = f"{old_date[:4]}.{old_date[4:6]}.{old_date[6:]}"
-        cursor.execute(f"select Дата from Главная where Дата=#{old_date[4:6]}/{old_date[6:]}/{old_date[:4]}#")
-        res = cursor.fetchall()
-        if (res != []):
-            print(f"{filename} already in DB")
-            return
-        sheetX = sheetX.dropna(axis=1,how='all')
-        
-        row_start = sheetX.loc[sheetX[sheetX.columns[0]] == "Код\nИнструмента"]._stat_axis[0]
-        row_end = sheetX.loc[sheetX[sheetX.columns[0]] == "Итого:"]._stat_axis[0]
-        row_tmp = row_start + 2
-        
-        while (row_tmp != row_end):
-            sheetX.loc[row_tmp]
-            for i in range(len(sheetX.loc[row_tmp]._values)):
-                if (sheetX.loc[row_tmp]._values[i] == "-"):
-                    sheetX.loc[row_tmp]._values[i] = "0"
+        for filename in reversed(os.listdir(directory)):
+            path_to_file = os.path.join(directory, filename)
+            xls = pd.ExcelFile(path_to_file)
+            sheetX = xls.parse(0) 
             
-            tovar = ""
-            for tov in tmp:
-                x = re.search(tov, sheetX.loc[row_tmp]._values[1], re.IGNORECASE)
-                if (x):
-                    tovar = x[0]
-                    if (tovar == tmp[0]):
-                        tovar = "СУГ"
-                    break
-            sheetX.loc[row_tmp]._values[6] = str(sheetX.loc[row_tmp]._values[6]).replace('.',',')
-            # TODO cursor.executemany(-, -)
-            cursor.execute("insert into Главная (КодИнструмента, НаименованиеИнструмента, БазисПоставки, ОбъемДоговоровЕИ, ОбъемДоговоровРуб, ИзмРынРуб, ИзмРынПроц, МинЦена, СреднЦена, МаксЦена, РынЦена, ЛучшПредложение, ЛучшСпрос, КоличествоДоговоров, Дата, Товар) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-                           (sheetX.loc[row_tmp]._values[0], sheetX.loc[row_tmp]._values[1], sheetX.loc[row_tmp]._values[2], sheetX.loc[row_tmp]._values[3],
-                            sheetX.loc[row_tmp]._values[4], sheetX.loc[row_tmp]._values[5], sheetX.loc[row_tmp]._values[6], sheetX.loc[row_tmp]._values[7],
-                            sheetX.loc[row_tmp]._values[8], sheetX.loc[row_tmp]._values[9], sheetX.loc[row_tmp]._values[10], sheetX.loc[row_tmp]._values[11], 
-                            sheetX.loc[row_tmp]._values[12], sheetX.loc[row_tmp]._values[13], date, tovar))
-            cursor.commit()
-            row_tmp += 1
+            if sheetX.empty:
+                print('File is empty!')
+                continue
             
-        # print(filename + " type = " + str(typeOfTable(sheetX)))
-        # print(xls.loc[xls[xls.columns[0]] == one[0][0]])
-        print(f"{filename} inserted in database")
-    cursor.close()
-    conn.close() 
-
+            old_date = re.search("\d{8}", filename)[0]
+            date = f"{old_date[:4]}.{old_date[4:6]}.{old_date[6:]}"
+            cursor.execute(f"select Дата from Главная where Дата=#{old_date[4:6]}/{old_date[6:]}/{old_date[:4]}#")
+            res = cursor.fetchall()
+            if (res != []):
+                print(f"{filename} already in DB")
+                return
+            sheetX = sheetX.dropna(axis=1,how='all')
+            
+            row_start = sheetX.loc[sheetX[sheetX.columns[0]] == "Код\nИнструмента"]._stat_axis[0]
+            row_end = sheetX.loc[sheetX[sheetX.columns[0]] == "Итого:"]._stat_axis[0]
+            row_tmp = row_start + 2
+            
+            while (row_tmp != row_end):
+                sheetX.loc[row_tmp]
+                for i in range(len(sheetX.loc[row_tmp]._values)):
+                    if (sheetX.loc[row_tmp]._values[i] == "-"):
+                        sheetX.loc[row_tmp]._values[i] = "0"
+                
+                tovar = ""
+                for tov in tmp:
+                    x = re.search(tov, sheetX.loc[row_tmp]._values[1], re.IGNORECASE)
+                    if (x):
+                        tovar = x[0]
+                        if (tovar == tmp[0]):
+                            tovar = "СУГ"
+                        break
+                sheetX.loc[row_tmp]._values[6] = str(sheetX.loc[row_tmp]._values[6]).replace('.',',')
+                # TODO cursor.executemany(-, -)
+                cursor.execute("insert into Главная (КодИнструмента, НаименованиеИнструмента, БазисПоставки, ОбъемДоговоровЕИ, ОбъемДоговоровРуб, ИзмРынРуб, ИзмРынПроц, МинЦена, СреднЦена, МаксЦена, РынЦена, ЛучшПредложение, ЛучшСпрос, КоличествоДоговоров, Дата, Товар) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                            (sheetX.loc[row_tmp]._values[0], sheetX.loc[row_tmp]._values[1], sheetX.loc[row_tmp]._values[2], sheetX.loc[row_tmp]._values[3],
+                                sheetX.loc[row_tmp]._values[4], sheetX.loc[row_tmp]._values[5], sheetX.loc[row_tmp]._values[6], sheetX.loc[row_tmp]._values[7],
+                                sheetX.loc[row_tmp]._values[8], sheetX.loc[row_tmp]._values[9], sheetX.loc[row_tmp]._values[10], sheetX.loc[row_tmp]._values[11], 
+                                sheetX.loc[row_tmp]._values[12], sheetX.loc[row_tmp]._values[13], date, tovar))
+                cursor.commit()
+                row_tmp += 1
+                
+            # print(filename + " type = " + str(typeOfTable(sheetX)))
+            # print(xls.loc[xls[xls.columns[0]] == one[0][0]])
+            print(f"{filename} inserted in database")
+        cursor.close()
+        conn.close() 
+    except pyodbc.Error as e:
+        print("DB not found")
+        
 def main():
     directory = "Downloads"
     getFilesFromAllPages("https://spimex.com/markets/oil_products/trades/results/?page=page-", directory)
